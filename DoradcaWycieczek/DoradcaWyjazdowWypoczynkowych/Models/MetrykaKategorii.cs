50885951ac5e66e6430c4bb93423989fb4f75a31
przemzas <przzason@gmail.com>
2014-05-31 00:11:52 +0200

    Pierwsza wersja komparatora.
    Sprowadzenie diagramu radarowego do postaci dwóch prostych.
    Wyliczenie średniej z wartości absolutnej różnic dla danych kryteriów.
    
    W wyniku tego otrzymujemy średnie oddalenie linii porównywanej od linii oryginalnej.

---
+++ DoradcaWycieczek/DoradcaWyjazdowWypoczynkowych/Models/MetrykaKategorii.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoradcaWyjazdowWypoczynkowych.Models
+{
+    public class MetrykaKategorii
+    {
+        public int Komfort { get; set; }
+        public int Zwiedzanie { get; set; }
+        public int Aktywnosc { get; set; }
+        public int Imprezowosc { get; set; }
+        public int BliskoNatury { get; set; }
+
+        public MetrykaKategorii(int Komfort, int Zwiedzanie, int Aktywnosc, int Imprezowosc, int BliskoNatury)
+        {
+            this.Komfort = Komfort;
+            this.Zwiedzanie = Zwiedzanie;
+            this.Aktywnosc = Aktywnosc;
+            this.Imprezowosc = Imprezowosc;
+            this.BliskoNatury = BliskoNatury;
+        }
+
+        double PorownajDopasowanie(MetrykaKategorii celPorownania)
+        {
+            int K = Math.Abs((celPorownania.Komfort - this.Komfort) * 100);
+            int Z = Math.Abs((celPorownania.Zwiedzanie - this.Zwiedzanie) * 100);
+            int A = Math.Abs((celPorownania.Aktywnosc - this.Aktywnosc) * 100);
+            int I = Math.Abs((celPorownania.Imprezowosc - this.Imprezowosc) * 100);
+            int BN = Math.Abs((celPorownania.BliskoNatury - this.BliskoNatury) * 100);
+
+            //zwraca średnie odchylenie
+            return ((K + Z + A + I + BN) / 5);
+        }
+    }
+}
